747e179e4f7125eabe5c68a7de937def50dd1d75
HADIDI Youssef <yhadidi@resoprint.fr>
2026-02-06 11:43:52 +0100

    worker path fix

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@ const nextConfig = {
   publicRuntimeConfig: {
     staticFolder: '/mnt/Truenas/truecloud/uploads',
   },
+  outputFileTracingIncludes: {
+    '/api/admin/cache/generate': ['./lib/workers/generateCacheWorker.mjs'],
+  },
   // Increase request body size limit for file uploads (5GB)
   api: {
     bodyParser: {
