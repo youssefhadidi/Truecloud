3330d3f042f50957859e0166c5f0a22c25c3c15e
HADIDI Youssef <yhadidi@resoprint.fr>
2026-02-11 11:10:26 +0100

    fix image player

--- components/files/viewers/ImageViewer.jsx
+++ components/files/viewers/ImageViewer.jsx
@@ -57,6 +57,14 @@ export function ImageViewer({ file, currentPath, getFileUrl, shareToken, sharePa
     <div className="relative w-full h-full flex items-center justify-center bg-gray-900">
       {/* Thumbnail as placeholder */}
       {hasThumbnail && !fullLoaded && <img src={thumbnailData.data} alt="" className="absolute inset-0 w-full h-full object-contain pointer-events-none" draggable={false} />}
+
+      {/* Loading spinner - visible while full image is loading */}
+      {!fullLoaded && (
+        <div className="absolute inset-0 flex items-center justify-center pointer-events-none z-10">
+          <div className="w-12 h-12 border-4 border-gray-600 border-t-white rounded-full animate-spin" />
+        </div>
+      )}
+
       {/* Full-res image on top */}
       <img
         ref={imgRef}
